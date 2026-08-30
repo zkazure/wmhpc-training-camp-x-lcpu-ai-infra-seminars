bf53041c9fb222acb175a83322671d1ffcc335e1
Zefeng Zheng <zhengzf23@mail2.sysu.edu.cn>
2026-08-30 11:49:57 +0000

    chore: add dir local for compile command

---
+++ assignment01/.dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((cuda-mode . ((compile-command . "cd ~/workspace/wmhpc-training-camp-x-lcpu-ai-infra-seminars/assignment01/cuda/ && make run/"))))
