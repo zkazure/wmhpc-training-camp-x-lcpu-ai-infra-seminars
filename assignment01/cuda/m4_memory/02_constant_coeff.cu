d97c923b98f4fad1f7ef7f0d3b09c11573cec1e8
Zefeng Zheng <zhengzf23@mail2.sysu.edu.cn>
2026-09-14 02:28:52 +0000

    feat: finish 4.3

--- assignment01/cuda/m4_memory/02_constant_coeff.cu
+++ assignment01/cuda/m4_memory/02_constant_coeff.cu
@@ -21,9 +21,18 @@ __global__ void poly_eval_global(const float *x, float *y, const float *coef,
     }
 }
 
+__constant__ float COEF[8];
 __global__ void poly_eval_const(const float *x, float *y, const float *coef,
                                 int n) {
     // TODO：从这里开始写（读 __constant__ COEF 的版本）
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < n) {
+        float xi = x[i];
+        float acc = 0.f;
+        for (int k = 7; k >= 0; k--) acc = acc * xi + COEF[k];
+        y[i] = acc;
+    }
+    
 }
 
 // ---------------- 以下是判测与计时，不要修改 ----------------
@@ -78,6 +87,7 @@ int main() {
     CUDA_CHECK(cudaMemcpy(d_coef, h_coef, sizeof(h_coef), cudaMemcpyHostToDevice));
 
     // TODO：把 h_coef 拷进你声明的 __constant__ 数组（cudaMemcpyToSymbol）。
+    cudaMemcpyToSymbol(COEF, h_coef, sizeof(h_coef));
 
     int threads = 256;
     int blocks = (n + threads - 1) / threads;
